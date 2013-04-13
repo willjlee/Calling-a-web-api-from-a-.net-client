55a9a14b44c65e9d179763be24ad2e45ce5eed51
William <willjlee@fake.email>
2013-04-12 20:23:19 -0400

    tutorial complete

---
+++ apiclient/apiclient/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apiclient
+{
+    class Contact
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public string Email { get; set; }
+        public string Twitter { get; set; }
+    }
+}
---
+++ apiclient/apiclient/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//non-default:
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+
+namespace apiclient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://contacttestthing.azurewebsites.net/"); //set base address
+
+            //add an accept header for json (tells server what format to use)
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            //add a contact
+            var newperson = new Contact() { Name = "newname", Address = "100 street st", City = "city", State="State", Zip=11111, Email="a@b.c", Twitter="twitter"};
+            Uri newpersonUri = null;
+
+            HttpResponseMessage response = client.PostAsJsonAsync("api/contacts", newperson).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                newpersonUri = response.Headers.Location;
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+
+
+            //get all contacts
+            response = client.GetAsync("api/contacts").Result;  // Blocking call!
+            if (response.IsSuccessStatusCode)
+            {
+
+                Console.WriteLine("call api/contacts");
+                // Parse the response body. Blocking!
+                var contacts = response.Content.ReadAsAsync<IEnumerable<Contact>>().Result;
+                foreach (var p in contacts)
+                {
+                    Console.WriteLine("{0}\t{1},{2}", p.Name, p.Address, p.City);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+
+            // Get a contact by ID
+            response = client.GetAsync("api/contacts/1").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("call api/contacts/1");
+                // Parse the response body. Blocking!
+                var contact = response.Content.ReadAsAsync<Contact>().Result;
+                Console.WriteLine("{0}\t{1},{2}", contact.Name, contact.Address, contact.City);
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+
+            // Delete the contact added in the beginning
+            response = client.DeleteAsync(newpersonUri).Result;
+            Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+
+
+            Console.ReadLine();//hold console open
+        }
+    }
+}
